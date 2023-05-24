41105ed4d99b58438fdba87756c2751e5f081f86
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-25 02:02:15 +0600

    [ADD]-shared-section-title-for-different-sections

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="my-8 md:w-4/12 mx-auto">
+      <p className="text-[#D99904] text-lg italic text-center mb-4">{subHeading}</p>
+      <div className="text-[#151515] text-4xl text-center uppercase border-y-4 border-[#E8E8E8] py-5">
+        {heading}
+      </div>
+    </div>
+  );
+};
+
+export default SectionTitle;
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -9,10 +9,15 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <>
+    <section>
+      <SectionTitle
+        subHeading={"---From 11:00am to 10:00pm---"}
+        heading={"ORDER ONLINE"}
+      ></SectionTitle>
       <Swiper
         slidesPerView={4}
         spaceBetween={30}
@@ -23,38 +28,39 @@ const Category = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
+        <SwiperSlide className="relative">
           <img src={slide1} alt="" />
-          <h3 className="text-center text-white text-3xl uppercase text-opacity-70 -mt-16">
-          Salads
+          <h3 className="text-white text-3xl uppercase text-opacity-70 absolute bottom-10 left-0 right-0 text-center">
+            salads
           </h3>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className="relative">
           <img src={slide2} alt="" />
-          <h3 className="text-white text-3xl uppercase text-opacity-70 text-center -mt-16">
-          pizzas
+          <h3 className="text-white text-3xl uppercase text-opacity-70 absolute bottom-10 left-0 right-0 text-center">
+            pizzas
           </h3>
         </SwiperSlide>
-        <SwiperSlide>
+
+        <SwiperSlide className="relative">
           <img src={slide3} alt="" />
-          <h3 className="text-center text-white text-3xl uppercase text-opacity-70 -mt-16">
-          Soups
+          <h3 className="text-white text-3xl uppercase text-opacity-70 absolute bottom-10 left-0 right-0 text-center">
+            soups
           </h3>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className="relative">
           <img src={slide4} alt="" />
-          <h3 className="text-center text-white text-3xl uppercase text-opacity-70 -mt-16">
-          sandwitch
+          <h3 className="text-white text-3xl uppercase text-opacity-70 absolute bottom-10 left-0 right-0 text-center">
+            sandwitch
           </h3>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className="relative">
           <img src={slide5} alt="" />
-          <h3 className="text-center text-white text-3xl uppercase text-opacity-70 -mt-16">
-          salads
+          <h3 className="text-white text-3xl uppercase text-opacity-70 absolute bottom-10 left-0 right-0 text-center">
+            salads
           </h3>
         </SwiperSlide>
       </Swiper>
-    </>
+    </section>
   );
 };
 
