ff33916ed7f77e1385c468c540146a2572be55b8
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-25 00:40:01 +0600

    [ADD]-create-simple-top-banner-react-responsive-carousel

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -6,26 +6,7 @@ const NavBar = () => {
           <a>Item 1</a>
         </li>
         <li tabIndex={0}>
-          <a className="justify-between">
-            Parent
-            <svg
-              className="fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-            </svg>
-          </a>
-          <ul className="p-2">
-            <li>
-              <a>Submenu 1</a>
-            </li>
-            <li>
-              <a>Submenu 2</a>
-            </li>
-          </ul>
+          <a>Parent</a>
         </li>
         <li>
           <a>Item 3</a>
@@ -35,7 +16,7 @@ const NavBar = () => {
   );
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-black bg-opacity-30  text-white max-w-screen-xl fixed z-10">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
