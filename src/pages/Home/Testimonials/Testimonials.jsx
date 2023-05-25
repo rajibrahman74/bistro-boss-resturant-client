2c9403a23eca8d7b9fb3a72322a9cb1a38ed7836
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-25 18:41:10 +0600

    [ADD&INSTALL]-create-parallax-and-add-testimonials-with-react-rating

---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json()) // Add parentheses after res.json
+      .then((data) => setReviews(data));
+  }, []);
+
+  return (
+    <section className="my-20">
+      <SectionTitle
+        subHeading="---What Our Clients Say---"
+        heading="TESTIMONIALS"
+      ></SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="m-24">
+              <Rating
+                className="text-center mx-auto my-5"
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <img
+                className="mx-auto w-24"
+                src="https://i.ibb.co/mD9tb3X/png-removebg-preview.png"
+                alt="png-removebg-preview"
+                border="0"
+              />
+              <p className="text-[#444444] text-lg text-center my-5">
+                {review.details}
+              </p>
+              <h3 className="text-[#CD9003] text-3xl text-center">
+                {review.name}
+              </h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
